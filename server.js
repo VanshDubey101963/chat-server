c6e878b4ad5efb0cb9160a52ee4c42502bac7ef8
cpp <vanshdubey101963@gmail.com>
2024-12-01 18:25:32 +0530

    initial socket and express setups

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {Server} = require('socket.io');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+server.listen(3000, () =>{
+    console.log("server running")
+});
+
+module.exports = {io}
+
+require('./socket/socket')
