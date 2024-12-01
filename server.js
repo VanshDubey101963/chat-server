92e51b73cf91e13d4cf0f7a4cdd51a559ff43a28
cpp <vanshdubey101963@gmail.com>
2024-12-01 18:55:01 +0530

    signup route configured in server.js file

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const {Server} = require('socket.io');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
 const signup = require('./routes/signup');
 
-require('dotenv').config();
-
+app.use('/signup', signup);
 const port = process.env.PORT;
-
 
 server.listen(port, () =>{
     console.log(`server running at ${port}`)
